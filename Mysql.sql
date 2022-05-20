4ba8570efa6bc393635603353ef98e37722ab8b4
Assylus <jose.macedo.jamj@gmail.com>
2022-05-20 17:26:20 -0300

    Adicionado primeiros passos em MySQL

---
+++ Mysql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pesssoas (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATE
+
+
+) 
+
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('José', '1997-07-06')
